0cc4f1b4313c9c165ba583f1962ca24db1fb2a00
Harsha4199 <devnaniharsha4@gmail.com>
2024-07-17 23:41:33 +0530

    Added Files

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.mylibrary1.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
+
+
     }
 }
